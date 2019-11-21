1ebdb9404cf8a3a8238be07d832e2a7ae1cbace4
Mohamed <mohamed.bahrini@hotmail.com>
2019-11-21 15:57:45 +0100

    create react front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Users from './components/users';
+import Home from './components/home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <Switch>
+            <Route path={'/home'} component={Home} />
+            <Route path="/users">
+              <Users />
+            </Route>
+          </Switch>
+        </React.Fragment>
+      </Router>
+    );
+  }
+
+  Users() {
+    return <Users />;
+  }
+
 }
 
 export default App;
